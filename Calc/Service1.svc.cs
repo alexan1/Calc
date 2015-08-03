e64cda9441324ead6a8a951a2295fdb27e7faaf9
Alexandre Konioukhov <alexan1@gmail.com>
2015-08-03 11:43:15 -0400

    initial

---
+++ Calc/Service1.svc.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Calc
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        //double number1, number2;
+        
+        string IService1.AddNumber(double number1, double number2)
+        {
+            return (number1 + number2).ToString();
+        }
+
+        string IService1.DivNumber(double number1, double number2)
+        {
+            return (number1 / number2).ToString();
+        }
+
+        string IService1.MultNumber(double number1, double number2)
+        {
+            return (number1 * number2).ToString();
+        }
+
+        string IService1.SubNumber(double number1, double number2)
+        {
+            return (number1 - number2).ToString();
+        }
+
+
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+    }
+}
---
+++ MyCalc/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //ServiceReference1.Service1Client Match1;
+
+        public MainWindow()
+        {
+            InitializeComponent();            
+            //Math1 = new ServiceReference1.Service1Client();
+        }
+
+        public ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            txtResult.Text = client.AddNumber(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));            
+        }
+
+        private void btnSub_Click(object sender, RoutedEventArgs e)
+        {
+            txtResult.Text = client.SubNumber(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));
+        }
+
+        private void btnMult_Click(object sender, RoutedEventArgs e)
+        {
+            txtResult.Text = client.MultNumber(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));
+        }
+
+        private void btnDiv_Click(object sender, RoutedEventArgs e)
+        {
+            txtResult.Text = client.DivNumber(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));
+        }
+    }
+}
